202e0b61f75cb65055b67c278ff596b8d29e5a38
manoj <roka.manoj12@gmail.com>
2025-02-11 13:01:35 +0545

    Feat: Add Static Data in Dashboard

--- frontend/resources/views/dashboard.blade.php
+++ frontend/resources/views/dashboard.blade.php
@@ -7,62 +7,32 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <table class="w-full border-collapse border border-gray-300 bg-black">
+            <div class="bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6">
+                    <table class="w-full border border-gray-300 bg-white text-black">
                         <thead>
                             <tr>
-                                <th class="border border-gray-300 p-2 text-center text-white" colspan="12">
+                                <th class="border border-gray-300 p-2 text-center text-lg font-semibold bg-gray-100" colspan="12">
                                     Process Live Feed
                                 </th>
                             </tr>
-                            <tr>
-                                <th class="border border-gray-300 p-2 text-left text-white" colspan="12" style="background-color: black;">
-                                    <span class="font-bold">Top:</span> <span class="ml-2">11:44:15 up 51 min, 1 user, load average: 1.00, 0.82, 0.48</span>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th class="border border-gray-300 p-2 text-left text-white" colspan="12" style="background-color: black;">
-                                    <span class="font-bold">Tasks:</span> <span class="ml-2">424 total, 2 running, 421 sleeping, 0 stopped, 1 zombie</span>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th class="border border-gray-300 p-2 text-left text-white" colspan="12" style="background-color: black;">
-                                    <span class="font-bold">%CPU(s):</span> <span class="ml-2">1.1 us, 0.6 sy, 0.0 ni, 98.3 id, 0.0 wa, 0.0 hi, 0.0 sl, 0.0 st</span>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th class="border border-gray-300 p-2 text-left text-white" colspan="12" style="background-color: black;">
-                                    <span class="font-bold">MIB Mem:</span> <span class="ml-2">31862.9 total, 21361.4 free, 4521.1 used, 5980.4 buff/cache</span>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th class="border border-gray-300 p-2 text-left text-white" colspan="12" style="background-color: black;">
-                                    <span class="font-bold">MiB Swap:</span> <span class="ml-2">2048.0 total, 2048.0 free, 0.0 used, 25912.8 avaul Mem</span>
-                                </th>
-                            </tr>
+                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>Top:</b> 11:44:15 up 51 min, 1 user, load average: 1.00, 0.82, 0.48 </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>Tasks:</b> 424 total, 2 running, 421 sleeping, 0 stopped, 1 zombie </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>%CPU(s):</b> 1.1 us, 0.6 sy, 0.0 ni, 98.3 id, 0.0 wa, 0.0 hi, 0.0 sl, 0.0 st </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>MIB Mem:</b> 31862.9 total, 21361.4 free, 4521.1 used, 5980.4 buff/cache </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>MiB Swap:</b> 2048.0 total, 2048.0 free, 0.0 used, 25912.8 avail Mem </th></tr>
 
-                            <tr>
-                                <th class="border border-gray-300 p-2 text-white">PID</th>
-                                <th class="border border-gray-300 p-2 text-white">USER</th>
-                                <th class="border border-gray-300 p-2 text-white">PR</th>
-                                <th class="border border-gray-300 p-2 text-white">NI</th>
-                                <th class="border border-gray-300 p-2 text-white">VIRT</th>
-                                <th class="border border-gray-300 p-2 text-white">RES</th>
-                                <th class="border border-gray-300 p-2 text-white">SHR</th>
-                                <th class="border border-gray-300 p-2 text-white">S</th>
-                                <th class="border border-gray-300 p-2 text-white">%CPU</th>
-                                <th class="border border-gray-300 p-2 text-white">%MEM</th>
-                                <th class="border border-gray-300 p-2 text-white">TIME+</th>
-                                <th class="border border-gray-300 p-2 text-white">COMMAND</th>
+                            <tr class="bg-gray-200">
+                                @foreach(['PID', 'USER', 'PR', 'NI', 'VIRT', 'RES', 'SHR', 'S', '%CPU', '%MEM', 'TIME+', 'COMMAND'] as $header)
+                                    <th class="border border-gray-300 p-2">{{ $header }}</th>
+                                @endforeach
                             </tr>
                         </thead>
-
                         <tbody>
                             @for ($j = 1; $j <= 20; $j++)
-                                <tr>
+                                <tr class="{{ $j % 2 === 0 ? 'bg-gray-50' : 'bg-white' }}">
                                     @for ($i = 1; $i <= 12; $i++)
-                                        <td class="border border-gray-300 p-2 text-white"></td>
+                                        <td class="border border-gray-300 p-2"></td>
                                     @endfor
                                 </tr>
                             @endfor
