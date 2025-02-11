19157016a1fef38599d10f30429a1cf9268069ec
manoj <roka.manoj12@gmail.com>
2025-02-11 13:10:09 +0545

    Feat: Add Static Data in Dashboard

--- frontend/resources/views/dashboard.blade.php
+++ frontend/resources/views/dashboard.blade.php
@@ -12,15 +12,15 @@
                     <table class="w-full border border-gray-300 bg-white text-black">
                         <thead>
                             <tr>
-                                <th class="border border-gray-300 p-2 text-center text-lg font-semibold bg-gray-100" colspan="12">
+                                <th class="border border-gray-300 p-2 text-center text-lg font-semibold" colspan="12">
                                     Process Live Feed
                                 </th>
                             </tr>
-                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>Top:</b> 11:44:15 up 51 min, 1 user, load average: 1.00, 0.82, 0.48 </th></tr>
-                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>Tasks:</b> 424 total, 2 running, 421 sleeping, 0 stopped, 1 zombie </th></tr>
-                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>%CPU(s):</b> 1.1 us, 0.6 sy, 0.0 ni, 98.3 id, 0.0 wa, 0.0 hi, 0.0 sl, 0.0 st </th></tr>
-                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>MIB Mem:</b> 31862.9 total, 21361.4 free, 4521.1 used, 5980.4 buff/cache </th></tr>
-                            <tr><th class="border border-gray-300 p-2 text-left bg-gray-100" colspan="12"> <b>MiB Swap:</b> 2048.0 total, 2048.0 free, 0.0 used, 25912.8 avail Mem </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left" colspan="12"> <b>Top: </b> 11:44:15 up 51 min, 1 user, load average: 1.00, 0.82, 0.48 </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left" colspan="12"> <b>Tasks: </b> 424 total, 2 running, 421 sleeping, 0 stopped, 1 zombie </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left" colspan="12"> <b>%CPU(s): </b> 1.1 us, 0.6 sy, 0.0 ni, 98.3 id, 0.0 wa, 0.0 hi, 0.0 sl, 0.0 st </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left" colspan="12"> <b>MIB Mem: </b> 31862.9 total, 21361.4 free, 4521.1 used, 5980.4 buff/cache </th></tr>
+                            <tr><th class="border border-gray-300 p-2 text-left" colspan="12"> <b>MiB Swap: </b> 2048.0 total,  2048.0 free, 0.0 used, 25912.8 avail Mem </th></tr>
 
                             <tr class="bg-gray-200">
                                 @foreach(['PID', 'USER', 'PR', 'NI', 'VIRT', 'RES', 'SHR', 'S', '%CPU', '%MEM', 'TIME+', 'COMMAND'] as $header)
